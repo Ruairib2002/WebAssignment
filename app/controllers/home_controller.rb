2ba9b8c1387352f6d4ac528ffb99a88e53f6269a
Ruairib2002 <ruairibreen2002@gmail.com>
2024-12-23 09:19:02 +0000

    Fixed error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @assignments = current_user.groups.joins(:assignments).where("assignments.due_date >= ?", Time.current).distinct
+    @assignments = Assignment.joins(group: :users)
+                             .where(groups: { id: current_user.group_ids })
+                             .where("assignments.due_date >= ?", Time.current)
+                             .distinct
   end
 end
