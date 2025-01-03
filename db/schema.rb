ade8f5e2fd22a826620743b53bf5cbbc8e9af0c5
Ruairib2002 <ruairibreen2002@gmail.com>
2025-01-03 16:43:50 +0000

    Added bio and profile picture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_09_211712) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_03_164309) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -122,6 +122,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_09_211712) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.text "bio"
+    t.string "profile_picture"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
