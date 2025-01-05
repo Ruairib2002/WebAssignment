b1e853eda1a8c8bb32095fdc08f55e9fb07d7cbf
Ruairib2002 <ruairibreen2002@gmail.com>
2025-01-05 13:27:17 +0000

    comment

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
     if params[:query].present?
-      # Search for students by full_name and groups by name
       @students = User.where("full_name LIKE ?", "%#{params[:query]}%")
       @groups = Group.where("name LIKE ?", "%#{params[:query]}%")
     else
