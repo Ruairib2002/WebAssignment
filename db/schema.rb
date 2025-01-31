3f749cced8b91502247266eb7024139c31e9e6fa
Ruairib2002 <ruairibreen2002@gmail.com>
2025-01-31 10:31:00 +0000

    Database seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_25_122322) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_31_102257) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -84,6 +84,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_25_122322) do
     t.datetime "updated_at", null: false
     t.integer "group_id"
     t.integer "recipient_id"
+  end
+
+  create_table "places", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,29 @@ User.find_or_create_by!(email: 'student@example.com') do |user|
   user.role = Role.find_by(role_name: 'student')
   user.full_name = 'Student User'
 end
+
+Place.create([
+  { name: "Dublin", latitude: 53.3498, longitude: -6.2603 },
+  { name: "Cork", latitude: 51.8985, longitude: -8.4756 },
+  { name: "Galway", latitude: 53.2707, longitude: -9.0568 },
+  { name: "Limerick", latitude: 52.6680, longitude: -8.6267 },
+  { name: "Kilkenny", latitude: 52.6584, longitude: -7.2475 },
+  { name: "Waterford", latitude: 52.2580, longitude: -7.1101 },
+
+  { name: "Belfast", latitude: 54.5973, longitude: -5.9301 },
+  { name: "Derry", latitude: 54.9960, longitude: -7.3190 },
+  { name: "Armagh", latitude: 54.3500, longitude: -6.6531 },
+  { name: "Lisburn", latitude: 54.5128, longitude: -6.0615 },
+  { name: "Newry", latitude: 54.1796, longitude: -6.3433 },
+  { name: "Antrim", latitude: 54.7039, longitude: -6.2153 },
+  { name: "Ballymena", latitude: 54.8716, longitude: -6.2763 },
+  { name: "Bangor", latitude: 54.6637, longitude: -5.6662 },
+  { name: "Portadown", latitude: 54.4708, longitude: -6.4533 },
+  { name: "Cookstown", latitude: 54.6467, longitude: -6.7408 },
+  { name: "Enniskillen", latitude: 54.3444, longitude: -7.6400 },
+  { name: "Carrickfergus", latitude: 54.7133, longitude: -5.1053 },
+  { name: "Newtownards", latitude: 54.5757, longitude: -5.6841 }
+])
+
+puts "Roles and Users created successfully"
+puts "Places seeded successfully"
