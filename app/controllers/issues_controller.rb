7690fd0df46e67f2fcee6a79b1d6288d463b634c
Ruairib2002 <ruairibreen2002@gmail.com>
2025-03-31 11:51:59 +0100

    New page for creating issue

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,19 +1,20 @@
 class IssuesController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create]
+  def new
+    @issue = Issue.new(latitude: params[:latitude], longitude: params[:longitude])
+  end
 
   def create
     @issue = Issue.new(issue_params)
-
     if @issue.save
-      render json: { success: true, issue: @issue }
+      redirect_to issues_path, notice: 'Issue created successfully.'
     else
-      render json: { success: false, errors: @issue.errors.full_messages }
+      render :new
     end
   end
 
   private
 
   def issue_params
-    params.require(:issue).permit(:issue_type, :latitude, :longitude)
+    params.require(:issue).permit(:description, :category, :latitude, :longitude)
   end
 end
